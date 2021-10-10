a8d3371cffb9ea7a2f6f7410684a9764cab57a8d
Muhammad Jawad Iqbal <jawadiqbal962@gmail.com>
2021-10-10 16:17:21 +0500

    Aded styled components instead of CSS file. Make a NAVbar for the website.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+const Container = styled.div`
+    height: 100vh;
+
+`
 const App = () => {
-  return <div>Hello world!</div>;
+  return <Container><Navbar/></Container>;
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+height: 50px;
+`;
+
+const Navbar = () => {
+    return (
+        <Container>
+            navbar
+        </Container>
+    )
+}
+
+export default Navbar
