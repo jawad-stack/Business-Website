02ebd398e756e7adbf3c85c9bb303cbd3541726a
Muhammad Jawad Iqbal <jawadiqbal962@gmail.com>
2021-10-10 16:23:59 +0500

    Changes in NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
 import styled from 'styled-components'
-const Container = styled.div`
-height: 50px;
+
+const Container = styled.div`jk
+    height: 50px;
 `;
 
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+
+`;
+
+const left = styled.div`
+    width: 60%;
+    display: flex;
+    align-items: center;
+`;
+
+const Logo = styled.h1`
+    font-weight : bold;
+    text-decoration: underlined crimson 
+`
+const Menu = styled.ul`
+    display: flex;
+    list-style: none;
+`;
+
+const MenuItem = styled.li`
+    margin-right: 30px;
+    font-size: 20px;
+    font-weight: bold;
+    color: gray;    
+`;
+
+const Button = styled.button`
+    border: 2px solid black;
+`
 const Navbar = () => {
     return (
         <Container>
-            navbar
+            <Wrapper>
+                <span>
+                    <Logo>Agency</Logo>
+                </span>
+                <Menu>
+                    <MenuItem>HOME</MenuItem>
+                    <MenuItem>Features</MenuItem>
+                    <MenuItem>Services</MenuItem>
+                    <MenuItem>Pricing</MenuItem>
+                    <MenuItem>Contact</MenuItem>
+                </Menu>
+                <button>Join Now</button>
+            </Wrapper>
         </Container>
     )
 }
