ad1533f2d86473cae03a97d8762176eb691e62c7
Muhammad Jawad Iqbal <jawadiqbal962@gmail.com>
2021-10-27 13:06:51 +0500

    some changes in serivices.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import styled from "styled-components";
-import Features from "./components/Features";
+import styled, { css } from "styled-components";
+import Contact from "./components/Contact";
+import Feature from "./components/Features";
+import Footer from "./components/Footer";
 import Intro from "./components/Intro";
 import Navbar from "./components/Navbar";
+import Price from "./components/Price";
+import Service from "./components/Services";
 
 const Container = styled.div`
   height: 100vh;
   overflow: hidden;
-  // position: relative;
+  position: relative;
 `;
 
-const IntroShape = styled.div`
-  width: 100%:
+const Shape = css`
+  width: 100%;
   height: 100%;
   position: absolute;
   top: 0;
   left: 0;
   z-index: -1;
-  clip-path: polygon(67% 0%, 100% 0%, 100% 100%, 55% 100%);
+`;
+
+const IntoShape = styled.div`
+  ${Shape}
+  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+  background-color: crimson;
+`;
+
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
+  background-color: pink;
+`;
+
+const ServiceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
+  background-color: #f88497;
+`;
+
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
   background-color: crimson;
 `;
 
 const App = () => {
+  const smallScreen = window.screen.width <= 480 ? true : false;
   return (
     <>
       <Container>
         <Navbar />
         <Intro />
-        <IntroShape />
+        <IntoShape />
       </Container>
       <Container>
-        <Features />
+        <Feature />
+        <FeatureShape />
+      </Container>
+      <Container>
+        <Service />
+        {!smallScreen && <ServiceShape />}
+      </Container>
+      <Container>
+        <Price />
+        <PriceShape />
+      </Container>
+      <Container>
+        <Contact />
+        <Footer />
       </Container>
     </>
   );
